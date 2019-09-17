890691a67237d69b98c1dbed36463e7b4ca7449d
Lorenzo-Giardi <lorenzo-giardi@outlook.com>
2019-09-17 13:12:00 +0200

    added train results for azure06 continuous DQN

--- other/function_plot.py
+++ other/function_plot.py
@@ -6,3 +6,20 @@ Created on Tue Sep 17 11:07:13 2019
 @author: lorenzo
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set_style("ticks")
+
+x = [1.2, 2]
+fx = [0, 0.2]
+gx = [0.2,0]
+
+plt.plot(x, fx, label='f(p)', c="#247afd")
+plt.plot(x, gx, label='g(p)', c="#fd8d49")
+plt.xlabel('Price')
+#plt.ylabel('...')
+plt.legend()
+plt.savefig('/home/lorenzo/Desktop/function_plots.png', dpi=600)
+plt.show()
