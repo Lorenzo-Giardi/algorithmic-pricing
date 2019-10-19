6778c77a2541da8651c88230e872f3c3f9220bb3
Lorenzo-Giardi <lorenzo-giardi@outlook.com>
2019-10-19 11:13:49 +0200

    Added plots for multisample rollout

--- train_results/Azure_cont_multisample/rollout/rollout_metrics.py
+++ train_results/Azure_cont_multisample/rollout/rollout_metrics.py
@@ -16,6 +16,11 @@ sns.set_style("ticks")
 import glob
 import os
 
+# ================ #
+#       DATA      ##
+# ================ #
+
+
 #### deltas averaged over agents ####
 path = '/home/lorenzo/algorithmic-pricing/train_results/Azure_cont_multisample/rollout/deltas_avgagt_csv'
 all_files = glob.glob(os.path.join(path, "*.csv"))
@@ -80,20 +85,51 @@ for i in all_files:
     
 prices_irf = pd.concat(prices_irf.values()).groupby(level=0).mean()
 
+# Print some general results
+deltas_mean = deltas_avgsess.mean()
+print(f'Overall deltas mean: {np.mean(np.array(deltas_avgts)):,.4f} and std: {np.std(np.array(deltas_avgts), axis=None):,.4f}')
+print(f'Agent0 deltas mean: {deltas_avgts.mean()[0]:,.4f} and std: {deltas_avgts.std()[0]:,.4f}')
+print(f'Agent1 deltas mean: {deltas_avgts.mean()[1]:,.4f} and std: {deltas_avgts.std()[1]:,.4f}')
 
 
+# ================ #
+#      PLOTS      ##
+# ================ #
 
+### Plot distribution of deltas
+
+sns.kdeplot(deltas_avgts, shade=True, cbar=True, cmap='Blues')
+plt.xlabel('Agent_0')
+plt.ylabel('Agent_1')
+plt.savefig('/home/lorenzo/Desktop/multisample-eval_density_deltas.png', dpi=600)
+plt.show()
+
+sns.kdeplot(deltas_avgts['Agent_0'], shade=True, label='Agent_0')
+sns.kdeplot(deltas_avgts['Agent_1'], shade=True, label='Agent_1')
+plt.xlabel('Profit gains (deltas)')
+plt.legend()
+plt.savefig('/home/lorenzo/Desktop/multisample-eval_density_deltas_2.png', dpi=600)
+plt.show()
+
+plt.plot(deltas_avgsess['Agent_0'], label='Agent_0', lw=0.8, alpha=0.9)
+plt.plot(deltas_avgsess['Agent_1'], label='Agent_1', lw=0.8, alpha=0.9)
+plt.xlabel('Timesteps')
+plt.ylabel('Profit gains (deltas)')
+plt.legend()
+plt.savefig('/home/lorenzo/Desktop/multisample-eval_plot_deltas.png', dpi=600)
+plt.show()
 
 
 
 ### plot impulse response functions
+
 plt.plot(deltas_irf['Agent_0'], label='Agent_0', c="#247afd")
 plt.plot(deltas_irf['Agent_1'], label='Agent_1', c="#fd8d49")
-plt.axhline(1, linestyle='dashed', c="#929591")
+#plt.axhline(1, linestyle='dashed', c="#929591")
 plt.xlabel('Timesteps')
 plt.ylabel('Profit gains (deltas)')
 plt.legend()
-plt.savefig('/home/lorenzo/Desktop/multisample-deltas-irf.png', dpi=600)
+plt.savefig('/home/lorenzo/Desktop/multisample_deltas_irf.png', dpi=600)
 plt.show()
 
 
@@ -102,5 +138,5 @@ plt.plot(prices_irf['Agent_1'], label='Agent_1', c="#fd8d49")
 plt.xlabel('Timesteps')
 plt.ylabel('Prices')
 plt.legend()
-plt.savefig('/home/lorenzo/Desktop/multisample-prices-irf.png', dpi=600)
+plt.savefig('/home/lorenzo/Desktop/multisample_prices-irf.png', dpi=600)
 plt.show()
